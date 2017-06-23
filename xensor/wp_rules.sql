690ad4a684ceec898391eef4f1e9e168a3f62c95
Xensor Nightshade <xensor@users.noreply.github.com>
2017-06-23 13:36:57 -0400

    Create wp_rules.sql

---
+++ xensor/wp_rules.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `wprh_rules` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` text COLLATE utf8_unicode_ci NOT NULL,
+ `entry` longtext COLLATE utf8_unicode_ci NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
